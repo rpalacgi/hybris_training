0888e033d3f5db032f0085f3c4ef0e1bee56f38c
Sergiy Mishkovets <sergiy_mishkovets@epam.com>
2015-02-18 11:16:02 +0100

    Backoffice extension has been added

---
+++ extensions/trainingbackoffice/backoffice/src/com/epam/training/services/TrainingbackofficeService.java
@@ -0,0 +1,10 @@
+package com.epam.training.services;
+
+
+public class TrainingbackofficeService
+{
+	public String getHello()
+	{
+		return "Hello";
+	}
+}
---
+++ extensions/trainingbackoffice/backoffice/testsrc/com/epam/training/TrainingbackofficeTest.java
@@ -0,0 +1,16 @@
+package com.epam.training;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class TrainingbackofficeTest
+{
+	@Test
+	public void testNothing()
+	{
+		assertTrue(true);
+		assertFalse(false);
+	}
+}
